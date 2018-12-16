78c6d247faaa72a2aeb8f0ebeeca2e8192f63acf
Ted Leary <tedleary@Teds-MacBook-Air.local>
2018-12-15 17:22:42 -0800

    home, app boilerplate

--- src/Home.js
+++ src/Home.js
@@ -1,63 +1,46 @@
-import React, { Component } from 'react';
-import { Link } from '@reach/router';
+import React, {Component} from 'react'
 
 class Home extends Component {
-  render() {
-    const { user } = this.props;
-
-    const biggerLead = {
-      fontSize: 1.4 + 'em',
-      fontWeight: 200
-    };
-
-    return (
+  render(){
+    const {user} = this.props;
+    return(
       <div className="container text-center">
         <div className="row justify-content-center">
+          Hello {user}
           <div className="col-10 col-md-10 col-lg-8 col-xl-7">
-            <div
-              className="display-4 text-primary mt-3 mb-2"
-              style={{
-                fontSize: 2.8 + 'em'
-              }}
-            >
+            <div className="display-4 text-primary mt-3 mx-2">
               Meeting Log
             </div>
-            <p className="lead" style={biggerLead}>
-              This simple app creates meetings, allows people to check
-              in, and picks random users to award giveaways. It's a
-              good example of a Single Page Application which includes
-              connection to a database and routing. It's a practical
-              way to learn <a href="https://reactjs.org/">React</a>{' '}
-              with <a href="https://firebase.google.com">Firebase</a>.
+            <p className="lead">
+              This simple app creates meetings, allows people to check in, and picks random users to award giveaways. It's a good example of a single page application which includes connection to a database and routing. It's a practical way to learn <a href="https://react.js.org/">React</a> with <a href="https://firebase.google.com">Firebase</a>
             </p>
 
             {user == null && (
               <span>
-                <Link
-                  to="/register"
-                  className="btn btn-outline-primary mr-2"
-                >
+                <a href="/register" className="btn btn-outline-primary mr-2">
                   Register
-                </Link>
-                <Link
-                  to="/login"
-                  className="btn btn-outline-primary mr-2"
-                >
+                </a>
+                <a href="/login" className="btn btn-outline-primary mr-2">
                   Log In
-                </Link>
+                </a>
+
               </span>
             )}
+
             {user && (
-              <Link to="/meetings" className="btn btn-primary">
+              <a href="/meetings" className="btn btn-primary">
                 Meetings
-              </Link>
+              </a>
             )}
-          </div>{' '}
-          {/* columns */}
+
+
+          </div>
+
         </div>
       </div>
-    );
+    )
   }
+
 }
 
 export default Home;

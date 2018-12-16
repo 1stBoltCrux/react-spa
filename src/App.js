90afa3b0c441fbc56bc3903622e9cd411c8cad24
Ted Leary <tedleary@Teds-MacBook-Air.local>
2018-12-15 17:40:57 -0800

    conditional switch for welcome component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 // Import React
 import React, { Component } from 'react';
-import Home from './Home'
+import Home from './Home';
+import Welcome from './Welcome';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      user: 'Ray'
+      user: 'Ted'
     }
   }
   render(){
     return(
       <div>
+        {this.state.user &&
+          <Welcome user={this.state.user}/>
+        }
+
         <Home user={this.state.user}/>
       </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,6 @@ class Home extends Component {
     return(
       <div className="container text-center">
         <div className="row justify-content-center">
-          Hello {user}
           <div className="col-10 col-md-10 col-lg-8 col-xl-7">
             <div className="display-4 text-primary mt-3 mx-2">
               Meeting Log
@@ -15,7 +14,7 @@ class Home extends Component {
               This simple app creates meetings, allows people to check in, and picks random users to award giveaways. It's a good example of a single page application which includes connection to a database and routing. It's a practical way to learn <a href="https://react.js.org/">React</a> with <a href="https://firebase.google.com">Firebase</a>
             </p>
 
-            {user == null && (
+            {!user && (
               <span>
                 <a href="/register" className="btn btn-outline-primary mr-2">
                   Register

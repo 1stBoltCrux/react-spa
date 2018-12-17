6671eae8567cd027ed77cb35695ed2b3673525cb
Ted Leary <tedleary@Teds-MacBook-Air.local>
2018-12-17 12:34:59 -0800

    add nav

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Welcome from './Welcome';
+import Navigation from './Navigation'
+import { Router } from '@reach/router'
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      user: 'Ted'
+      user: ''
     }
   }
   render(){
@@ -17,6 +19,7 @@ class App extends Component {
         {this.state.user &&
           <Welcome user={this.state.user}/>
         }
+        <Navigation user={this.state.user}/>
 
         <Home user={this.state.user}/>
       </div>
---
+++ src/Navigation.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { FaUsers } from 'react-icons/fa'
+
+class Navigation extends Component {
+  render(){
+    const {user} = this.props;
+    return(
+
+      <nav className="site-nav family-sans navbar navbar-expand bg-primary navbar-dark higher">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">
+            <FaUsers className="mr-1"/>  Meeting Log
+          </a>
+          <div className="navbar-nav ml-auto">
+            {user && (
+              <span className="d-flex">
+                <a className="nav-item nav-link" href="/login">
+                  log out
+                </a>
+                <a className="nav-item nav-link" href="/meetings">
+                  meetings
+                </a>
+              </span>
+            )}
+
+            {!user && (
+              <span className="d-flex">
+                <a className="nav-item nav-link" href="/login">
+                  log in
+                </a>
+                <a className="nav-item nav-link" href="/register">
+                  register
+                </a>
+              </span>
+
+            )}
+
+
+          </div>
+        </div>
+      </nav>
+
+
+    )
+  }
+
+}
+
+export default Navigation;
